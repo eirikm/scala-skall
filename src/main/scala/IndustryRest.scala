ce0f8daccc238f112377cfa9c04dec39e07ff6cb
eirmelan <eirik.meland@finn.no>
2014-05-21 22:04:09 +0200

    Flyttet Plan ut i eget trait

--- src/main/scala/IndustryRest.scala
+++ src/main/scala/IndustryRest.scala
@@ -1,27 +1,33 @@
+import unfiltered.filter.Plan
+import unfiltered.filter.Plan.Intent
 import unfiltered.request._
 import unfiltered.response._
 
-object IndustryRest
-  extends IndustryRepoComponent {
+object IndustryRest {
 
   def main(args: Array[String]) {
-    unfiltered.jetty.Http(1337).filter {
-      unfiltered.filter.Planify {
-        case GET(Path(Seg("industry" :: industryId :: Nil))) =>
-          industryRepo.getById(industryId).map {
-            industry =>
-              Ok ~> JsonContent ~> ResponseString(toJson(industry))
-          }.getOrElse(NotFound)
+    unfiltered.jetty.Http(1337).filter(IndustryPlan).run()
+  }
+}
 
-        case GET(Path(Seg("industry" :: Nil))) =>
-          Ok ~> JsonContent ~>
-            ResponseString(
-              industryRepo.getAll map(toJson(_)) mkString("[", ", ", "]"))
-      }
-    }.run()
+object IndustryPlan
+  extends Plan
+  with IndustryRepoComponent {
+
+  override def intent: Intent = {
+    case GET(Path(Seg("industry" :: industryId :: Nil))) =>
+      industryRepo.getById(industryId).map {
+        industry =>
+          Ok ~> JsonContent ~> ResponseString(toJson(industry))
+      }.getOrElse(NotFound)
+
+    case GET(Path(Seg("industry" :: Nil))) =>
+      Ok ~> JsonContent ~>
+        ResponseString(
+          industryRepo.getAll map(toJson(_)) mkString("[", ", ", "]"))
   }
 
-  def toJson(i: Industry): String = {
+  private def toJson(i: Industry): String = {
     s"""{ 'id' = "${i.id}",
        |  'name' = "${i.name}"
        |}""".stripMargin  }
